2d050caa663fc9861de539e2dbf0f282a554fa19
santimn2000 <130979889+santimn2000@users.noreply.github.com>
2024-01-23 09:19:05 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'mysql://santimn:root@localhost/gesjuegos'
+
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://santimn:rootroot@db4free.net/iesdaw45'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
